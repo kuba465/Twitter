184cd49b0362975a52ce7fbb3127f4b02adef8b1
kuba465 <jakub.k.paluszkiewicz@gmail.com>
2017-08-01 20:15:10 +0200

    Performing search services and search form

--- search_form_service_other_pages.php
+++ search_form_service_other_pages.php
@@ -4,11 +4,11 @@ if (isset($_POST['search'])) {
     if (!isset($_POST['userName']) || is_null($_POST['userName']) || strlen($_POST['userName']) <= 0) {
         echo "Zła nazwa użytkownika.";
     } else {
-        include 'src/User.php';
-        include 'config.php';
+        include_once 'src/User.php';
+        include_once 'config.php';
         $foundUser = User::loadUserByUsername($conn, $_POST['userName']);
         if ($foundUser !== false) {
-            header("Location: user_profile.php?id=".$foundUser->getId());
+            header("Location: ../profiles/user_profile.php?id=" . $foundUser->getId());
         }
     }
 }
